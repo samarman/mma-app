81b1e3a50db8e9cb70bcf401e280d281f4237ad4
samarman <97samarman@gmail.com>
2023-01-25 15:47:51 +0000

    Committed to the repo

---
+++ Apps/mma-app/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from "../app";
+import Header from "../app";
+
+function App() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+  
+  export default App;
+
